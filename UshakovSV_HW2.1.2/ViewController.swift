5b0e46916e1ecc80b312a28784514d1026692ff9
mrbrazz <mrbrazz@yandex.ru>
2021-07-07 16:36:47 +0300

    add let lightOn and lightOff

--- UshakovSV_HW2.1.2/ViewController.swift
+++ UshakovSV_HW2.1.2/ViewController.swift
@@ -8,23 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IB Outlets
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
     
+    // MARK: - Private Properties
+    private let lightOn: CGFloat = 1.0
+    private let lightOff: CGFloat = 0.3
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        startButton.layer.cornerRadius = 10
+        
         redLight.layer.cornerRadius = 60
         yellowLight.layer.cornerRadius = 60
         greenLight.layer.cornerRadius = 60
-        redLight.alpha = 0.3
-        yellowLight.alpha = 0.3
-        greenLight.alpha = 0.3
-        
-        
+
+        redLight.alpha = lightOff
+        yellowLight.alpha = lightOff
+        greenLight.alpha = lightOff
     }
-
-
+    @IBAction func startButtonPressed() { startButton.setTitle("Next", for: .normal)
+        redLight.alpha = 1.0
 }
-
+}
